0d4d02d3debbbb639b2d15bdb38cca96101b9019
Nata <melvia.nr@gmail.com>
2022-09-19 07:39:26 +0500

    metu and routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'quest';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter.reducer';
+import { MyCounterComponent } from './my-counter/my-counter.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { PlayComponent } from './play/play.component';
+import { GamesComponent } from './games/games.component';
+import { CurrentGameComponent } from './current-game/current-game.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, MyCounterComponent, HomeComponent, AboutComponent, NotFoundComponent, PlayComponent, GamesComponent, CurrentGameComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,    
+    StoreModule.forRoot({ count: counterReducer }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
